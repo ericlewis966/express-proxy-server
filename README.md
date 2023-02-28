ff6359af3ee002988f66e8d6fe3de834b562b07c
cyber-storm-200712 <leonsummer2007@gmail.com>
2023-02-28 06:58:42 -0800

    modulized

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-# express-proxy-server
+## simple-express-proxy🗜📡
 
-##Initialize Guid
+#### This is simple express proxy web server using https protocol.
+Following is simple guide and usage example for quick start.
+
+1. Install package in your application
+`
+npm install simple-express-proxy
+`
+2. Write code in your entry point code section
+
+```javascript
+const createProxy = require("simple-express-proxy");
+const fs = require("fs");
+
+// https server security configuration
+const  privateKey  =  fs.readFileSync('./secure/key.pem', 'utf-8');
+const  certificate  =  fs.readFileSync('./secure/cert.pem', 'utf-8');
+
+// proxy setup configuration
+const passphrase = 'your custom password'; //security key
+const target = 'https://google.com'; //target address to serve
+const whitelist = ['0.0.0.0', '95.180.41.194']; // ip list to allow access to proxy service
+
+// create proxy server
+const proxy = createProxy(privateKey, certificate, passphrase, target, whitelist);
+
+// host proxy
+proxy.listen((port, host) => {
+	console.log(`https proxy server is running on port ${port} and host ${host}`)
+})
 
 ```
-//create 'secure' directory and generate key pair (cert.pem, key.pem)
-```
 
-```
-npm install
-```
+3. Host your application . 💻
 
-```
-// config host and project settings
-```
-
-
-<img style="width:100%" class="gb-lazyimage gb-my-6" src="https://nc-resourcecenter.com/wp-content/uploads/2020/12/CDN-ProxyServers-SEOArticle-03-09-min-1200x837.png" alt="">
+#### Contact to cyberstorm2007@gmail.com, enemymaycry2007@gmail.com to get more supports and utility packages
--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const https = require('https'),
-    httpProxy = require('http-proxy'),
-    fs = require('fs');
+    httpProxy = require('http-proxy');
 
+const createProxyServer = (privateKey, certificate, passphrase, target, whitelist) => {
+    const credentials = { key: privateKey, cert: certificate, passphrase };
 
-const config = require('./config.json');
+    const proxy = httpProxy.createServer({
+        changeOrigin: true,
+        ssl: credentials,
+        secure: true,
+    });
 
-const privateKey = fs.readFileSync('./secure/key.pem', 'utf-8');
-const certificate = fs.readFileSync('./secure/cert.pem', 'utf-8');
+    const server = https.createServer({ ...credentials, rejectUnauthorized: false }, (req, res) => {
+        if (whitelist.indexOf(req.socket.remoteAddress) >= 0) {
+            proxy.web(req, res, { target: target, changeOrigin: true });
+            console.log(`User IP: ${req.socket.remoteAddress} was accessed.`);
+        } else {
+            res.end("Sorry, You are not in whitelist");
+            console.log(`User IP: ${req.socket.remoteAddress} was kicked.`)
+        }
+    })
 
-const credentials = { key: privateKey, cert: certificate, passphrase: 'jug200712' };
+    return server
+}
 
-const proxy = httpProxy.createServer({
-    changeOrigin: true,
-    ssl: credentials,
-    secure: true,
-});
-
-
-const server = https.createServer({ ...credentials, rejectUnauthorized: false }, (req, res) => {
-    if (config.whitelist.indexOf(req.socket.remoteAddress) >= 0) {
-        proxy.web(req, res, { target: config.target, changeOrigin: true });
-        console.log(`User IP: ${req.socket.remoteAddress} was accessed.`);
-    } else {
-        res.end("Sorry, You are not in whitelist");
-        console.log(`User IP: ${req.socket.remoteAddress} was kicked.`)
-    }
-})
-server.listen(config.port, config.host, () => {
-    console.log(`Proxy server running on port ${config.port}, IP ${config.host}`)
-});
+module.exports = createProxyServer
